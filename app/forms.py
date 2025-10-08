68759b6b356c997382d1ce023d03c421dfa55b01
Yu Xiang <167894937+YuXiang1105@users.noreply.github.com>
2025-10-08 23:46:00 +0100

    almost finished, needs more decoration

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from wtforms import StringField,SubmitField,TextAreaField
 from wtforms.validators import DataRequired
 
 class MessageForm(FlaskForm):
      name = StringField('name', validators=[DataRequired()])
      class_imput = StringField('class', validators=[DataRequired()])
-     origin = StringField('origin', validators=[DataRequired()])
+     origin = StringField('origin')
      danger = StringField('danger', validators=[DataRequired()])
-     description = StringField('description', validators=[DataRequired()])
+     description = TextAreaField('description', validators=[DataRequired()])
      submit = SubmitField('submit')
--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from .forms import MessageForm
 main = Blueprint("main", __name__)
 
 aliens = [
     {
-        "name": "xenomorph",
-        "danger": "extreme",
-        "class": "parasyte",
-        "origin": "open space, not confirmed",
-        "description": "The Xenomorph is a highly aggressive extraterrestrial species with a unique life cycle and a deadly hunting instinct. It is known for its biomechanical appearance, acid blood, and ability to adapt to various environments. The Xenomorph is a formidable predator that poses a significant threat to any life form it encounters.",
+        "Name": "Xenomorph",
+        "Danger": "extreme",
+        "Class": "parasyte",
+        "Origin": "open space, not confirmed",
+        "Description": "The Xenomorph is a highly aggressive extraterrestrial species with a unique life cycle and a deadly hunting instinct. It is known for its biomechanical appearance, acid blood, and ability to adapt to various environments. The Xenomorph is a formidable predator that poses a significant threat to any life form it encounters.",
     },
     {
-        "name": "Gravis",
-        "danger": "Low",
-        "class": "Mineral",
-        "origin": "Asteroids",
-        "description": "Rocky creatures that manipulate gravity around them to move and defend themselves."
+        "Name": "Naytibas",
+        "Danger": "Extreme",
+        "Class": "Bio-mechanical",
+        "Origin": "Earth (post-human epoch)",
+        "Description": "he Naytibas are biomechanical alien organisms that invaded Earth, annihilating most of humanity. Their bodies fuse flesh and machinery, capable of regenerating and adapting to combat. They are driven by a hive-like will and worship an ancient entity known as the Elder Naytiba."
     },
     {
-        "name": "Luminaris",
-        "danger": "Moderate",
-        "class": "Energy",
-        "origin": "Nebulas",
-        "description": "Translucent beings that emit light and communicate through pulses of energy."
+        "Name": "Lord of Cinder",
+        "Danger": "Extreme",
+        "Class": "Humanoid",
+        "Origin": "Lordran",
+        "Description": "Specie ascended by absorbing a legendary artifact in Lordran, the First flame. After ascending, any specimen becomes extremely powerful, being considered even gods in their planets"
+    },
+    {
+        "Name": "Radiance",
+        "Danger": "Extreme",
+        "Class": "Parasyte",
+        "Origin": None,
+        "Description": "A specimen from the kindom of Hallownest, part of a planet of intelligent insects. Survives by invading the mind of other living being, and inducing them to a dream. If the subject is in the dream, Radiance has full control of the body, being able to destroy entire kindoms"
     }
+    
 ]
 
 @main.route('/')
@@ -53,14 +61,15 @@ def form():
     if form.validate_on_submit():
         
         new_alien = {
-            "name": form.name.data,
-            "danger": form.danger.data,
-            "class": form.class_imput.data,
-            "origin": form.origin.data,
-            "description": form.description.data
+            "Name": form.name.data,
+            "Danger": form.danger.data,
+            "Class": form.class_imput.data,
+            "Origin": form.origin.data,
+            "Description": form.description.data
         }
         
         aliens.append(new_alien)
+        return redirect(url_for('main.species'))
         
         
     return render_template('form.html', class_imput = class_imput,name = name,
